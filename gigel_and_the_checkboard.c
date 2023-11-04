a3963d83596c781de3c6586f9ac326ab40d42865
student <rconstantinescu2006>
2023-11-04 18:11:52 +0200

    55p

--- gigel_and_the_checkboard.c
+++ gigel_and_the_checkboard.c
@@ -10,18 +10,16 @@ int modul_de(int numar)
 
 int transf_neg(int numar, int d)
 {
-	if (numar >= 0)
-		return numar;
-	else
-		return transf_neg(numar + d, d);
+	while (numar < 0)
+		numar += d;
+	return numar;
 }
 
 int transformare(int numar, int baza)
 {
-	if (numar < baza)
-		return numar;
-	else
-		return transformare(numar - baza, baza);
+	while (numar >= baza)
+		numar -= baza;
+	return numar;
 }
 
 void conversie(int i)
@@ -41,12 +39,14 @@ void conversie(int i)
 	}
 }
 
-int matrice_de_frecventa[1001][1001];
-
 int main(void)
 {
 	int n, tabla[1001][1001], i, j;
 	long long k = 0;
+	int matrice_de_frecventa[1001][1001];
+	for (int ind = 0; ind < 1001; ind++)
+		for (int ind2 = 0; ind2 < 1001; ind2++)
+			matrice_de_frecventa[ind][ind2] = 0;
 	scanf("%d", &n);
 	for (i = 0; i < n; i++)
 		for (j = 0; j < n; j++)
@@ -54,14 +54,14 @@ int main(void)
 	i = 0;
 	j = 0;
 	while (tabla[i][j] != 0 && matrice_de_frecventa[i][j] == 0) {
-	if ((i + j) % 2 == 0) {
-		matrice_de_frecventa[i][j] = 1;
-		k += modul_de(tabla[i][j]);
-		j = transformare(j + transf_neg(tabla[i][j], n), n);
-	} else {
-		matrice_de_frecventa[i][j] = 1;
-		k += modul_de(tabla[i][j]);
-		i = transformare(i + transf_neg(tabla[i][j], n), n);
+		if ((i + j) % 2 == 0) {
+			matrice_de_frecventa[i][j] = 1;
+			k += modul_de(tabla[i][j]);
+			j = transformare(j + transf_neg(tabla[i][j], n), n);
+		} else {
+			matrice_de_frecventa[i][j] = 1;
+			k += modul_de(tabla[i][j]);
+			i = transformare(i + transf_neg(tabla[i][j], n), n);
 		}
 	}
 	printf("%lld\n", k);
--- nomogram.c
+++ nomogram.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int games;
     scanf("%d", &games);
     while(games) {
