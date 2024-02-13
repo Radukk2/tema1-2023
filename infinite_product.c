9498258cf6428ee524868daf831d43762e0bdbfb
Radukk2 <115574097+Radukk2@users.noreply.github.com>
2024-02-13 15:40:21 +0200

    Update infinite_product.c

--- infinite_product.c
+++ infinite_product.c
@@ -1,52 +1,48 @@
 #include <stdio.h>
 #include <math.h>
 
-int convert_to_base_10(int x)
+long long convert_8_to_10(long long n)
 {
-    int copie = x, nr_cif = 0;
-    while (copie) {
-        nr_cif++;
-        copie /= 10;
-    }
-    int new_number = 0, ctr = 0;
-    while (nr_cif != ctr) {
-       new_number += pow(8, ctr) * (x % 10);
-       ctr++;
-       x /= 10;
-    }
-    return new_number;
-}   
+	long long i = 0, new = 0;
+	while (n > 0) {
+		new = new + (n % 10) * pow(8, i);
+		i++;
+		n = n / 10;
+		}
+	return new;
+}
 
-int main() {
-    int lung;
-    double norm1 = 0, norm2 = 0;
-    scanf("%d", &lung);
-    int v1[lung], v2[lung];
-    int prod = 0;
-    for (int i = 0; i < lung; i++) {
-        int x, y;
-        scanf("%d%d", &x, &y);
-        v1[i] = convert_to_base_10(x);
-        v2[i] = convert_to_base_10(y);
-        prod += v1[i] * v2[i];
-        norm1 += v1[i] * v1[i];
-        // norm1 = sqrt(norm1);
-        norm2 += v2[i] * v2[i];
-        // norm2 = sqrt(norm2);
-    }
-    for (int i = 0; i < lung - 1; i++)
-        for (int j = i; j < lung; j++) {
-            if (v1[i] < v1[j]) {
-                int aux = v1[i];
-                v1[i] = v1[j];
-                v1[j] = aux;
-            }
-            if (v2[i] < v2[j]) {
-                int aux = v2[i];
-                v2[i] = v2[j];
-                v2[j] = aux;
-            }
-        }
-    printf("%d\n%d %d\n%.7f %.7f", prod, v1[1], v2[1], norm1, norm2);
-    return 0;
+int main(void)
+{
+	long long N, a, b, ps = 0;
+	long long ma1 = -1, ma2 = -1, mb1 = -1, mb2 = -1;
+	double norma_a, norma_b, n1 = 0, n2 = 0;
+	scanf("%lld", &N);
+	while (N > 0) {
+		scanf("%lld%lld", &a, &b);
+		a = convert_8_to_10(a);
+		b = convert_8_to_10(b);
+		ps += a * b;
+		N = N - 1;
+		if (a > ma1) {
+			ma2 = ma1;
+			ma1 = a;
+		}
+		if (b > mb1) {
+			mb2 = mb1;
+			mb1 = b;
+		}
+		if (a > ma2 && a < ma1)
+			ma2 = a;
+		if (b > mb2 && b < mb1)
+			mb2 = b;
+		n1 += (double)a * (double)a;
+		n2 += (double)b * (double)b;
+	}
+	norma_a = sqrt(n1);
+	norma_b = sqrt(n2);
+	printf("%lld\n", ps);
+	printf("%lld %lld\n", ma2, mb2);
+	printf("%.7lf %.7lf\n", norma_a, norma_b);
+	return 0;
 }
